e1123f6bb42880d45c1d1fcdcc0a3408beb6aa66
midnight33 <gra33@bk.ru>
2023-02-28 18:30:35 +0300

    Задача 3 готово

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
-﻿static void Main()
-        {
+﻿        {
                 Console.WriteLine("Введите число : ");
                 int a = Convert.ToInt32(Console.Read());
                 if (a % 2 == 0) Console.WriteLine("Четное");
